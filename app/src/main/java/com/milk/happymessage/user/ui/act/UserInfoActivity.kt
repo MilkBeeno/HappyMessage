9d52713a8388073e49550716df2c7ce88e620914
MilkBeeno <milkbeeno@gmail.com>
2022-12-02 11:53:32 +0800

    用户信息页面 UI 修改一

--- app/src/main/java/com/milk/happymessage/user/ui/act/UserInfoActivity.kt
+++ app/src/main/java/com/milk/happymessage/user/ui/act/UserInfoActivity.kt
@@ -57,14 +57,14 @@ class UserInfoActivity : AbstractActivity() {
 
     private fun initializeView() {
         immersiveStatusBar()
-        binding.flHeader.statusBarPadding()
+        binding.flHeaderToolbar.statusBarPadding()
         binding.root.navigationBarPadding()
-        binding.headerToolbar.showArrowBack()
+        binding.ivBack.setOnClickListener(this)
         binding.ivReport.setOnClickListener(this)
         binding.link.tvCopy.setOnClickListener(this)
-        binding.ivUserNext.setOnClickListener(this)
-        binding.basic.llMessage.setOnClickListener(this)
-        binding.basic.llFollow.setOnClickListener(this)
+        binding.llNext.setOnClickListener(this)
+        binding.tvMessage.setOnClickListener(this)
+        binding.llFollow.setOnClickListener(this)
         binding.flVideo.setOnClickListener(this)
         binding.mlImage.setOnClickListener(this)
         binding.link.flLinkLocked.setOnClickListener(this)
@@ -80,11 +80,10 @@ class UserInfoActivity : AbstractActivity() {
         userInfoViewModel.loadUserInfoStatusFlow.collectLatest(this) {
             if (it) {
                 binding.lvLoading.gone()
-                binding.llUserNext.visible()
-                binding.basic.root.visible()
+                binding.llUserBottom.visible()
                 binding.link.root.visible()
                 binding.llMedia.visible()
-                setUserBasic()
+                setUserBasicInfo()
                 setUserFollow()
                 setUserLink()
                 val videoEmpty = setUserVideo()
@@ -128,33 +127,32 @@ class UserInfoActivity : AbstractActivity() {
     }
 
     private fun setUserFollow() {
-        binding.basic.llFollow.visible()
-        val params = binding.basic.ivFollow.layoutParams as LinearLayoutCompat.LayoutParams
+        binding.llFollow.visible()
+        val params = binding.ivFollow.layoutParams as LinearLayoutCompat.LayoutParams
         if (userInfoViewModel.getUserInfoModel().targetIsFollowed) {
-            binding.basic.tvFollow.gone()
-            binding.basic.ivFollow.setImageResource(R.drawable.user_info_followed)
-            binding.basic.llFollow.setBackgroundResource(R.drawable.shape_user_info_followed)
+            binding.tvFollow.gone()
+            binding.ivFollow.setImageResource(R.drawable.user_info_followed)
+            binding.llFollow.setBackgroundResource(R.drawable.shape_user_info_followed)
             params.marginStart = dp2px(8f).toInt()
             params.marginEnd = dp2px(8f).toInt()
-            binding.basic.ivFollow.layoutParams = params
+            binding.ivFollow.layoutParams = params
         } else {
-            binding.basic.tvFollow.visible()
-            binding.basic.ivFollow.setImageResource(R.drawable.user_info_un_follow)
-            binding.basic.llFollow.setBackgroundResource(R.drawable.shape_user_info_un_follow)
+            binding.tvFollow.visible()
+            binding.ivFollow.setImageResource(R.drawable.user_info_un_follow)
+            binding.llFollow.setBackgroundResource(R.drawable.shape_user_info_un_follow)
             params.marginStart = dp2px(10f).toInt()
             params.marginEnd = 0
-            binding.basic.ivFollow.layoutParams = params
+            binding.ivFollow.layoutParams = params
         }
     }
 
     @SuppressLint("SetTextI18n")
-    private fun setUserBasic() {
+    private fun setUserBasicInfo() {
         val userInfo = userInfoViewModel.getUserInfoModel()
         ImageLoader.Builder()
             .loadAvatar(userInfo.targetAvatar, userInfo.targetGender)
-            .target(binding.basic.ivUserAvatar)
+            .target(binding.ivUserAvatar)
             .build()
-        binding.basic.ivUserGender.updateGender(userInfo.targetGender)
         binding.tvUserName.text = userInfo.targetName
         binding.tvUserId.text = "ID : ".plus(userInfo.targetIdx)
         binding.tvUserBio.text = userInfo.targetBio
@@ -261,7 +259,7 @@ class UserInfoActivity : AbstractActivity() {
         super.onMultipleClick(view)
         when (view) {
             binding.ivReport -> reportDialog.show()
-            binding.basic.llFollow -> {
+            binding.llFollow -> {
                 if (Account.userLogged) {
                     loadingDialog.show()
                     userInfoViewModel.changeFollowedStatus()
@@ -277,7 +275,7 @@ class UserInfoActivity : AbstractActivity() {
                 cmb.setPrimaryClip(ClipData.newPlainText(label, link))
                 showToast(string(R.string.user_info_copy_success))
             }
-            binding.ivUserNext -> {
+            binding.llNext -> {
                 FireBaseManager.logEvent(FirebaseKey.CLICK_THE_NEXT)
                 create(this)
                 finish()
@@ -292,7 +290,7 @@ class UserInfoActivity : AbstractActivity() {
                     )
                 }
             }
-            binding.basic.llMessage -> {
+            binding.tvMessage -> {
                 FireBaseManager
                     .logEvent(FirebaseKey.CLICK_MESSAGE_ON_PROFILE_PAGE)
                 if (Account.userLogged) {
